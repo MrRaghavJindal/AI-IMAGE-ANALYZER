df68c272ea09cb01b57b28d32deb4e95e22149a5
Raghav Jindal <raghavjindal345@gmail.com>
2025-08-19 16:12:42 +0530

    add

--- project/src/main.tsx
+++ project/src/main.tsx
@@ -3,6 +3,23 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+// Import the virtual PWA register helper
+import { registerSW } from 'virtual:pwa-register'
+
+// Register Service Worker
+// const updateSW = registerSW({
+//   onNeedRefresh() {
+//     // optional: show a "refresh" button if a new version is available
+//     console.log('New content available. Please refresh.')
+//   },
+//   onOfflineReady() {
+//     // optional: notify user they can use app offline
+//     console.log('App ready to work offline.')
+//   },
+// })
+
+registerSW()
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
